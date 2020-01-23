fc67421c3fabf5aa882f9dab70a84ce1912e802a
kamalNayak86 <knayak@aecordigital.com>
2020-01-23 18:05:38 +0530

    Updates

---
+++ precache-manifest.ac5cf56477013c2d50fa09ad58954429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8a6df9a48e4e66fcdeae7fb41c51f3",
+    "url": "/react-starter/index.html"
+  },
+  {
+    "revision": "c1c052b298fcccf4ac64",
+    "url": "/react-starter/static/css/main.539b450d.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/react-starter/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "c1c052b298fcccf4ac64",
+    "url": "/react-starter/static/js/main.742c4d0c.chunk.js"
+  },
+  {
+    "revision": "3dcb92dd2c2eab5c2f49",
+    "url": "/react-starter/static/js/runtime~main.6737c922.js"
+  }
+]);
